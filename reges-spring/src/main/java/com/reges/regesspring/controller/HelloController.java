10ff5e2b4206c72ccbe5da933d2da2a945d2142e
Administrador <antonio.alex@hotmail.com>
2022-04-26 16:44:48 -0300

    spring-boot

---
+++ reges-spring/src/main/java/com/reges/regesspring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.reges.regesspring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+    
+
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return "Olá mundo!";
+    }
+    
+}
+
